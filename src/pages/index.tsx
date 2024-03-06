cd01f768353967229055086a03b468e0e372be52
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-03-06 17:23:10 +0900

    <feat: 비밀번호 및 비밀번호 확인 기능 구현>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { z } from "zod";
 import { Button } from "@/components/ui/button"
-import { motion } from "framer-motion";
 import {
   Form,
   FormControl,
@@ -23,7 +22,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
 import {
   Select,
   SelectContent,
@@ -31,13 +29,13 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
 
 type registerInputType = z.infer<typeof formSchema>;
 
 export default function SignUp() {
 
-  const [pageStep, setPageStep] = useState<number>(0);
+  const { toast } = useToast();
 
   const form = useForm<registerInputType>({
     resolver: zodResolver(formSchema),
@@ -52,32 +50,50 @@ export default function SignUp() {
     }
   });
 
-  console.log("입력값 => ", form.watch());
+  console.log("입력값 => ", form.watch("username"));
 
-  const onClickNextStepHandler = () => {
-    setPageStep(pageStep + 1);
-  };
+  const onSubmit = (data: registerInputType) => { // 비밀번호 확인 작업
+    const {
+      username,
+      nickname,
+      id,
+      email,
+      select,
+      password,
+      confirmPassword
+    } = data;
+    const formData = {
+      username,
+      nickname,
+      id,
+      email: email + select,
+      password,
+      confirmPassword
+    }
 
-  const onClickPrevStepHandler = () => {
-    setPageStep(pageStep - 1);
-  };
-
-  const onSubmit = () => { // 비밀번호 확인 작업
-
+    if (password !== confirmPassword) {
+      toast({
+        title: "비밀번호가 일치하지 않습니다.",
+        variant: "destructive",
+        duration: 1000,
+      });
+      return;
+    }
+    alert(JSON.stringify(formData, null, 4));
   };
 
   return (
     <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-      <Card className={cn("w-[550px]")}>
+      <Card className={cn("w-[450px]")}>
         <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>Deploy your new project in one-click.</CardDescription>
+          <CardTitle>계정 등록하기</CardTitle>
+          <CardDescription>승재의 세상으로 빠져들기 위한 준비가 필요해요!</CardDescription>
         </CardHeader>
-        <CardContent className="space-y-5">
+        <CardContent className="space-y-3">
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(onSubmit)}
-              className="space-y-5">
+              className="space-y-3">
               <FormField
                 control={form.control}
                 name="username"
@@ -101,21 +117,6 @@ export default function SignUp() {
                     <FormLabel>닉네임</FormLabel>
                     <FormControl>
                       <Input placeholder="스파르타" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      <FormMessage />
-                    </FormDescription>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="id"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>아이디</FormLabel>
-                    <FormControl>
-                      <Input placeholder="sparta1234" {...field} />
                     </FormControl>
                     <FormDescription>
                       <FormMessage />
@@ -150,10 +151,14 @@ export default function SignUp() {
                               </SelectTrigger>
                             </FormControl>
                             <SelectContent position="popper">
-                              <SelectItem value="naver.com">naver.com</SelectItem>
-                              <SelectItem value="daum.net">daum.net</SelectItem>
-                              <SelectItem value="gmail.com">gmail.com</SelectItem>
-                              <SelectItem value="hanmail.net">hanmail.net</SelectItem>
+                              <SelectItem value="@naver.com">naver.com</SelectItem>
+                              <SelectItem value="@daum.net">daum.net</SelectItem>
+                              <SelectItem value="@gmail.com">gmail.com</SelectItem>
+                              <SelectItem value="@hanmail.net">hanmail.net</SelectItem>
+                              <SelectItem value="@nate.com">nate.com</SelectItem>
+                              <SelectItem value="@hotmail.com">hotmail.com</SelectItem>
+                              <SelectItem value="@outlook.com">outlook.com</SelectItem>
+                              <SelectItem value="@icloud.com">icloud.com</SelectItem>
                             </SelectContent>
                           </Select>
                         )}
@@ -163,12 +168,54 @@ export default function SignUp() {
                   </FormItem>
                 )}
               />
-              <div className="flex justify-end gap-10">
-                <Button
-                  type="button"
-                  className={cn({ hidden: pageStep === 2 })}
-                  onClick={onClickNextStepHandler}>
-                  다음 단계로
+              <FormField
+                control={form.control}
+                name="id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>아이디</FormLabel>
+                    <FormControl>
+                      <Input placeholder="sparta1234" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      <FormMessage />
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>비밀번호</FormLabel>
+                    <FormControl>
+                      <Input type={"password"} {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      <FormMessage />
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>비밀번호 확인</FormLabel>
+                    <FormControl>
+                      <Input type={"password"} {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      <FormMessage />
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-start gap-10">
+                <Button className="bg-blue-400 text-white hover:bg-blue-300" type="submit">
+                  계정 등록하기
                 </Button>
               </div>
             </form>
